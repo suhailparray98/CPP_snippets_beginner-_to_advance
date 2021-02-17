192d6f6085205a33b0fee33210ccb2d00fa11343
SUHAIL AHMAD PARRAY <37875320+suhailparray98@users.noreply.github.com>
2021-02-17 12:46:34 +0530

    Create if.cpp

---
+++ Beginner level/CONTROL STATEMENT/if.cpp
@@ -0,0 +1,15 @@
+/*in this program try operators like < or >
+and both in single and dual mode with if statements like if ((4<5) && or || (5>2))
+try also not equal to operator (!=)
+and try out some expressions also */
+#include<iostream>
+using namespace std;
+int main()
+{
+    if(2<3)  //(3<=2))
+    {
+        cout<<"soub is one of the best programmer in the world !"<<endl;
+    }
+
+return 0;
+}
